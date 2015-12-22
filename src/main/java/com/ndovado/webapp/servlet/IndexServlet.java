48bcdca456baa8516ab642a07396e591cb1c099d
antonio <antonio.schiazza@gmail.com>
2015-12-22 15:54:00 +0100

    terzo commit

--- src/main/java/com/ndovado/webapp/servlet/IndexServlet.java
+++ src/main/java/com/ndovado/webapp/servlet/IndexServlet.java
@@ -2,7 +2,6 @@ package com.ndovado.webapp.servlet;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -28,9 +27,7 @@ public class IndexServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//response.getWriter().append("Served at: ").append(request.getContextPath());
-		RequestDispatcher rd = request.getRequestDispatcher("/MainPage.jsp");
-		rd.forward(request, response);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
